afc465e9a5c33de9f519c4c111ed08af23a5bf38
Code Master <info@codemaster.ltd>
2025-08-14 10:19:50 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,15 @@
 fx_version 'cerulean'
+game 'gta5'
 
-game 'gta5' 
+author 'Code Master & Crip Developments'
+name 'British Plates'
+version '1.0.0'
+description [[
+Customizable UK-style vehicle number plates for GTA V.
+Generates realistic DVLA plates based on real-world year codes,
+with support for multiple formats and randomized letters.
+]]
 
 client_scripts {
     'client.lua'
 }
-
-name 'British Plates'
-description 'Customisable british plates.'
-author 'Owen-M'
